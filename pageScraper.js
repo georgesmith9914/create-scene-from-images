ad308454077c2b81c5a5052fe0ab5014687bc135
Sanjeev Kumar <georgesmith9914@gmail.com>
2022-07-03 21:52:01 +0200

    Updated for puppeteer selector

--- pageScraper.js
+++ pageScraper.js
@@ -1,21 +1,26 @@
 const scraperObject = {
-    url: 'http://books.toscrape.com',
+    //url: 'https://linktr.ee/draplin',
+	url: 'https://linktr.ee/katyperry',
     async scraper(browser){
-        let page = await browser.newPage();
-		console.log(`Navigating to ${this.url}...`);
-		// Navigate to the selected page
-		await page.goto(this.url);
-		// Wait for the required DOM to be rendered
-		await page.waitForSelector('.page_inner');
-		// Get the link to all the required books
-		let urls = await page.$$eval('section ol > li', links => {
-			// Make sure the book to be scraped is in stock
-			links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== "In stock")
-			// Extract the links from the data
-			links = links.map(el => el.querySelector('h3 > a').href)
-			return links;
-		});
-		console.log(urls);
+        try{
+            let page = await browser.newPage();
+            console.log(`Navigating to ${this.url}...`);
+            // Navigate to the selected page
+            await page.goto(this.url);
+            // Wait for the required DOM to be rendered
+            await page.waitForSelector('#__next');
+            // Get the link to all the required books
+            let urls = await page.$$eval("[data-testid='StyledContainer']", links => {
+                // Make sure the book to be scraped is in stock
+                //links = links.filter(link => link.querySelector('.instock.availability > i'))
+                // Extract the links from the data
+                links = links.map(el => el.querySelector('a').href)
+                return links;
+            });
+            console.log(urls);
+        }catch(e){
+            console.log(e);
+        }
     }
 }
 
